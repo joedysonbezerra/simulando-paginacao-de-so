13815f116853dee2160ba20c020b59b5893f7a07
Joedyson Bezerra <joedysonbezerra@gmail.com>
2019-06-17 19:41:07 -0300

    Compartilhamento de valor

---
+++ src/SharedValue.java
@@ -0,0 +1,16 @@
+class SharedValue {
+   private int value;
+
+   public SharedValue(int value) {
+      this.value = value;
+   }
+
+   public int getValue() {
+      return value;
+   }
+
+   public void setValue(int value) {
+      this.value = value;
+   }
+
+}
