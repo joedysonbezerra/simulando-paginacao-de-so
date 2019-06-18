87a2c640284cd61ccc89e9851f1262c3ad67c97f
joedysonbezerra <joedysonbezerra@gmail.com>
2019-06-18 20:20:08 -0300

    Implementação da mémoria virtual, OK

--- src/VirtualMemory.java
+++ src/VirtualMemory.java
@@ -1,23 +1,23 @@
 import java.util.ArrayList;
 
 class VirtualMemory {
-   ArrayList<VirtualPage> vm;
-   SharedValue v;
+   ArrayList<VirtualPage> virtualMemory;
 
    public VirtualMemory(int size) {
-      this.vm = new ArrayList<VirtualPage>(size);
+      this.virtualMemory = new ArrayList<VirtualPage>();
 
       for (int i = 0; i < size; i++) {
          VirtualPage page = new VirtualPage();
-         this.vm.add(page);
+         this.virtualMemory.add(page);
       }
-      SharedValue t = new SharedValue(1);
-      v = t;
-      System.out.println(t.getValue());
-      System.out.println(v.getValue());
-      t.setValue(2);
-      System.out.println(t.getValue());
-      System.out.println(v.getValue());
+   }
 
+   public void printVirtualMemory() {
+      System.out.println(">>>>> Memória Virtual <<<<<<");
+      for (VirtualPage page : virtualMemory) {
+         System.out.println("[ " + page.isReference() + ", " + page.isModify() + ", " + page.isPresent() + ", "
+               + page.pageFrameNumber() + " ],");
+      }
+      System.out.println(">>>>> Memória Virtual - FIM <<<<<<");
    }
 }
