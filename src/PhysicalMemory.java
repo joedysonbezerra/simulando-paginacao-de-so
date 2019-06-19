5a086c6a977163035dcf824b0b2084bc34440922
joedysonbezerra <joedysonbezerra@gmail.com>
2019-06-18 21:07:16 -0300

    Implementação da mémoria física, OK

--- src/PhysicalMemory.java
+++ src/PhysicalMemory.java
@@ -1,5 +1,40 @@
 import java.util.ArrayList;
+import java.util.Vector;
 
 class PhysicalMemory {
+   Vector<Integer> physicalMemory = new Vector<Integer>();
+
+   PhysicalMemory(int size) {
+      for (int i = 0; i < size; i++) {
+         physicalMemory.add(null);
+      }
+
+   }
+
+   public ArrayList isFull() {
+      ArrayList response = new ArrayList();
+      response.add(true);
+      response.add(-1);
+
+      int address = 0;
+
+      for (Integer element : physicalMemory) {
+         if (element == null) {
+            response.set(0, false);
+            response.set(1, address);
+            break;
+         }
+         address++;
+      }
+
+      return response;
+   }
+
+   public void printPhysicalMemory() {
+      System.out.println(">>>>> Memória Ram <<<<<<");
+      System.out.print("[ ");
+      physicalMemory.forEach(value -> System.out.print(value + " "));
+      System.out.print("]");
+   }
 
 }
