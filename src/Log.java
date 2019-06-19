1ec78084d4b95d932857399eefe114bf5e7278c0
joedysonbezerra <joedysonbezerra@gmail.com>
2019-06-19 01:56:52 -0300

    Implementação do Log, OK

--- src/Log.java
+++ src/Log.java
@@ -4,17 +4,20 @@ class Log implements Runnable {
 
    VirtualMemory virtualMemory;
    PhysicalMemory ramMemory;
+   HardDisk HD;
 
-   Log(VirtualMemory vm, PhysicalMemory rm) {
+   Log(VirtualMemory vm, PhysicalMemory rm, HardDisk hd) {
       this.virtualMemory = vm;
       this.ramMemory = rm;
+      this.HD = hd;
    }
 
    @Override
    public void run() {
       while (true) {
-         virtualMemory.printVirtualMemory();
-         ramMemory.printPhysicalMemory();
+         this.virtualMemory.printVirtualMemory();
+         this.ramMemory.printPhysicalMemory();
+         this.HD.printHardDisk();
          try {
             Thread.sleep(5000);
          } catch (InterruptedException e) {
