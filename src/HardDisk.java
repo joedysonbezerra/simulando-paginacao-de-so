b567236cad6eaa0e1a627408b09005760d97426e
joedysonbezerra <joedysonbezerra@gmail.com>
2019-06-19 01:57:30 -0300

    Implementação do HD, OK

---
+++ src/HardDisk.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+class HardDisk {
+   ArrayList<ArrayList<Integer>> hd = new ArrayList<ArrayList<Integer>>();
+
+   public void setHD(int size) {
+      ArrayList<Integer> processData = new ArrayList<Integer>();
+      for (int i = 0; i < size; i++) {
+         processData.add(new Random().nextInt(26));
+      }
+
+      hd.add(processData);
+   }
+
+   public Integer getProcessData(int processAddress, int dataAddress) {
+      return this.hd.get(processAddress).get(dataAddress);
+   }
+
+   public void printHardDisk() {
+      int i = 0;
+      for (ArrayList<Integer> processData : this.hd) {
+         System.out.println(">>>>> Dados do Programa em execução - " + i + " <<<<<");
+         System.out.print("[ ");
+         processData.forEach(value -> System.out.print(value + " "));
+         System.out.println("]");
+         i++;
+      }
+   }
+
+}
