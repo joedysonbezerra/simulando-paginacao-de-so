189b71a21a32dedff4741b9e86f08ebc7010972b
joedysonbezerra <joedysonbezerra@gmail.com>
2019-06-21 01:12:24 -0300

    Process, Ok

--- src/Process.java
+++ src/Process.java
@@ -3,8 +3,14 @@ import java.util.Objects;
 class Process extends Thread {
    private int pid;
    private int firstAddressMemory;
-   private MemoryManagementUnit mmu;
+   private MMUI mmu;
    private String operations[];
+
+   Process(int pid, int firstAddressMemory, MMUI mmu) {
+      this.pid = pid;
+      this.firstAddressMemory = firstAddressMemory;
+      this.mmu = mmu;
+   }
 
    public void getInput() {
       String input = "5-R,6-R,7-R,2-W,1-W,3-R,6-W,1-W,0-R,1-W,0-W,4-R,5-R,7-W,1-R,2-W";
@@ -14,9 +20,9 @@ class Process extends Thread {
    public void executeOperations() {
       for (String operation : this.operations) {
          if (Objects.equals(operation.substring(2), "R")) {
-            mmu.read((firstAddressMemory + Integer.parseInt(operation.substring(1))));
+            mmu.read(firstAddressMemory, pid);
          } else {
-            mmu.write((firstAddressMemory + Integer.parseInt(operation.substring(1))));
+            mmu.write(firstAddressMemory, pid);
          }
       }
    }
